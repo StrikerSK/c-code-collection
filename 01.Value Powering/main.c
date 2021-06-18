6a8bb64d2f48aaec797bd51c3b4ef8471d2ad620
Teh|Striker <kristianhanus@gmail.com>
2021-06-18 23:07:37 +0200

    INIT

---
+++ 01.Value Powering/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void powerValue(int *a, int b) {
+    int tmpValue = *a;
+    for (int i = 1; i < b; i++) {
+        *a = *a * tmpValue;
+    }
+}
+
+int main() {
+    int a, b;
+
+    printf("Input initial value:");
+    scanf("%d", &a);
+
+    printf("Input power value:");
+    scanf("%d", &b);
+
+    printf("Values before powerValue(): a = %d, b = %d\n", a, b);
+    powerValue(&a, b);
+    printf("Values before powerValue(): a = %d, b = %d\n", a, b);
+
+    return 0;
+}
+
